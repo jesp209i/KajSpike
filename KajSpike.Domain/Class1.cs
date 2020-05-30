84dab1938915031d6ede4cd5d3e3596958c01633
Jesper Albrecht Madsen <jesp209i@edu.ucl.dk>
2020-05-30 10:28:09 +0200

    Add project files.

---
+++ KajSpike.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace KajSpike.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ KajSpike.Framework/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace KajSpike.Framework
+{
+    public class Class1
+    {
+    }
+}
